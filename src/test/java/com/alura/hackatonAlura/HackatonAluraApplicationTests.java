d94d28a57953fdfc8909e58c0ac5ba645e6181b7
Agueda Talavera <aguedatala09@hotmail.com>
2025-12-16 13:06:06 -0600

    Proyecto Inicial hackaton Alura

---
+++ src/test/java/com/alura/hackatonAlura/HackatonAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.hackatonAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
